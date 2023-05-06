32d482f2a25eca5d03566c7fb7654451250d0b5a
Mario Alberto Gorostegui <mario.gorostegui@hotmail.com>
2023-05-06 13:25:15 -0300

    se bajo el proyecto

---
+++ sistema_coder/control_estudios/views.py
@@ -0,0 +1,49 @@
+"""
+URL configuration for sistema_coder project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.shortcuts import render
+
+def lista_estudiantes (request):
+    contexto = {
+        "estudiantes": [
+            {"nombre": "Emanuel", "apellido": "Dautel"},
+            {"nombre": "Manuela", "apellido": "Gomez"},
+            {"nombre": "Ivan", "apellido": "Tomasevich"},
+            {"nombre": "Carlos", "apellido": "Perez"},
+        ]
+     }
+    http_responde=render(
+        request = request,
+        template_name= 'control_estudios/lista_estudiantes.html',
+        context = contexto,
+    )
+    return http_responde
+
+def lista_cursos (request):
+    contexto = {
+        "cursos": [
+            {"nombre": "Phyton", "comision": "33245"},
+            {"nombre": "FrontEnd", "comision": "55698"},
+            {"nombre": "SQL", "comision": "45887"},
+        ]
+     }
+    http_responde=render(
+        request = request,
+        template_name= 'control_estudios/lista_cursos.html',
+        context = contexto,
+    )
+    return http_responde
+
---
+++ sistema_coder/sistema_coder/views.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def saludar(request):
+    saludo = "Hola querido usuario"
+    pagina_html = HttpResponse(saludo)
+    return pagina_html
+
+
+def saludar_con_fecha(request):
+    hoy = datetime.now()
+    saludo = f"Hola querido usuario, fecha: {hoy.day}/{hoy.month}"
+    pagina_html = HttpResponse(saludo)
+    return pagina_html
+
+
+def saludar_a_usuario(request, nombre):
+    texto = f"Hola {nombre}"
+    pagina_html = HttpResponse(texto)
+    return pagina_html
+
+def saludar_con_html (request):
+    contexto ={
+        "usuario" : "Mario A"
+    }
+    http_responde=render(
+        request = request,
+        template_name= 'control_estudios/base.html',
+        context = contexto,
+    )
+    return http_responde
