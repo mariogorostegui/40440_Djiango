a809773087e11cfe1ff733c9a420596607ad9685
Mario Alberto Gorostegui <mario.gorostegui@hotmail.com>
2023-05-07 17:23:31 -0300

    Agregado panel administratico

--- sistema_coder/control_estudios/models.py
+++ sistema_coder/control_estudios/models.py
@@ -4,16 +4,20 @@ from django.db import models
 class Curso(models.Model):
     nombre = models.CharField(max_length=64)  # Equivalente de str
     comision = models.IntegerField()  # Equivalent de int
-
+    
+    def __str__(self):
+        return f"{self.nombre} | {self.comision}"
 
 class Estudiante(models.Model):
     apellido = models.CharField(max_length=256)
     nombre = models.CharField(max_length=256)
-    email = models.EmailField()
-    telefono = models.CharField(max_length=20)
+    email = models.EmailField(blank=True) #blank=true lo hace no obligatorio
+    telefono = models.CharField(max_length=20,blank=True) #en los campos numeros se debe poner null=true
     dni = models.CharField(max_length=32)
     fecha_nacimiento = models.DateField()
 
+    def __str__(self):
+        return f"{self.nombre}  {self.apellido}"
 
 class Profesor(models.Model):
     apellido = models.CharField(max_length=256)
@@ -23,6 +27,9 @@ class Profesor(models.Model):
     fecha_nacimiento = models.DateField()
     profesion = models.CharField(max_length=128)
     bio = models.TextField()
+    
+    def __str__(self):
+        return f"{self.nombre}  {self.apellido}"
 
 
 class Entregable(models.Model):
